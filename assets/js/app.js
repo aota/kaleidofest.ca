bd2f376a3342772b6018c6993faad3289933b312
Eli Dupuis <elidupuis@gmail.com>
2014-11-03 21:58:18 -0700

    update dropbox link fixing logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function(){
   // make sure Dropbox links download immediately
   $('a[href*="dropbox.com"]').not('[href$="?dl=1"]').each(function(){
-    this.href += '?dl=1';
+    this.href = this.href.replace(/\?dl=\d/, '') + '?dl=1';
   });
 });
